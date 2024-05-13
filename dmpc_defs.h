2d1b6723b282bc0f222eb3e8d752693746c4eb98
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2024-05-13 12:44:03 +0200

    Moved matrices definitions to source file

--- dmpc_defs.h
+++ dmpc_defs.h
@@ -15,21 +15,21 @@
 #define DMPC_CONFIG_SCALE			1.000000f
 
 /* Number of model states and augmented states */
-#define DMPC_CONFIG_NXM			2
-#define DMPC_CONFIG_NXA			3
+#define DMPC_CONFIG_NXM			1
+#define DMPC_CONFIG_NXA			2
 
 /* Prediction, control and constraint horizon */
-#define DMPC_CONFIG_NP			5
-#define DMPC_CONFIG_NC			5
+#define DMPC_CONFIG_NP			10
+#define DMPC_CONFIG_NC			10
 #define DMPC_CONFIG_NR			1
-#define DMPC_CONFIG_NLAMBDA		4
+#define DMPC_CONFIG_NLAMBDA		2
 
 /* Number of inputs and outputs */
 #define DMPC_CONFIG_NU			1
 #define DMPC_CONFIG_NY			1
 
 /* Number of external disturbances */
-#define DMPC_CONFIG_ND			0
+#define DMPC_CONFIG_ND			1
 
 /* Size of control vector */
 #define DMPC_CONFIG_NC_x_NU		DMPC_CONFIG_NC * DMPC_CONFIG_NU
@@ -38,6 +38,6 @@
 #define DMPC_CONFIG_NU_CTR		1
 
 /* State constraints */
-#define DMPC_CONFIG_NXM_CTR		1
+#define DMPC_CONFIG_NXM_CTR		0
 
 #endif /* DMPC_DEFS_H_ */
